740a77945bd1e49a81716c05c0949852fd2ed128
Boris <boris_619@hotmail.com>
2024-04-16 14:24:33 +0200

    update career info

--- src/assets/data/career/enCareerData.js
+++ src/assets/data/career/enCareerData.js
@@ -4,7 +4,7 @@ const enCareerData = [
     year: 2017,
     title: "Computer Technician",
     subtitle: "Port of Marín - Port Authority of Marín and Ría de Pontevedra",
-    details: "Implemented deployment services and automated installations, configured monitoring systems and developed scripts. Managed data migrations, installed operating systems and provided technical support to users.",
+    details: "More than 50 computer devices. Implemented deployment services and automated installations, configured monitoring systems and developed scripts. Managed data migrations, installed operating systems and provided technical support to users.",
     techs: ['WDS', 'Zabbix', 'PowerShell', 'Backups', 'Ubuntu Server', 'Windows Server']
   },
   {
@@ -12,7 +12,7 @@ const enCareerData = [
     year: 2018,
     title: "Computer Systems Technician",
     subtitle: "Plexus Tech - Pontevedra and O Salnés Health Area",
-    details: "Responsible for tutoring and supervising students in internships, user maintenance in Windows environment, configuration of medical terminals, image deployment and data migration. Also, provided technical support and resolved incidents to users in the work environment.",
+    details: "More than 1000 computer devices. Responsible for tutoring and supervising students in internships, user maintenance in Windows environment, configuration of medical terminals, image deployment and data migration. Also, provided technical support and resolved incidents to users in the work environment.",
     techs: ['AD-DS', 'Backups', 'TCP/IP']
   },
   {
@@ -20,7 +20,7 @@ const enCareerData = [
     year: 2020,
     title: "Web Developer and Administrator",
     subtitle: "Depot230",
-    details: "Adeptly configured and managed WordPress plugins, demonstrated proficiency in PHP programming, handled web service administration and managed cPanel operations. Provided training and tutoring to clients, developed complete websites, as well as designed and edited advertising images and logos.",
+    details: "More than 250 websites. Adeptly configured and managed WordPress plugins, demonstrated proficiency in PHP programming, handled web service administration and managed cPanel operations. Provided training and tutoring to clients, developed complete websites, as well as designed and edited advertising images and logos.",
     techs: ['Wordpress', 'PHP', 'cPanel', 'SEO', 'phpMyAdmin', 'HTML', 'CSS', 'JavaScript', 'GIMP']
   },
   {
@@ -28,7 +28,7 @@ const enCareerData = [
     year: 2021,
     title: "Web Application Developer and Systems Administrator",
     subtitle: "Liceo Casino of Pontevedra",
-    details: "From application development and IT consulting to data retrieval and computer hardware maintenance. Additionally, executed backup and restoration strategies, offered network assistance and managed email service transitions.",
+    details: "More than 1500 users. From application development and IT consulting to data retrieval and computer hardware maintenance. Additionally, executed backup and restoration strategies, offered network assistance and managed email service transitions.",
     techs: ['Backups', 'HTML', 'CSS', 'JavaScript', 'Vite', 'NPM', 'React.js', 'Tailwind', 'phpMyAdmin', 'MongoDB', 'MariaDB', 'MySQL', 'Node.js', 'Git', 'GitHub', 'ESLint', 'WordPress', 'SEO', 'Bash', 'PowerShell']
   },
   {
@@ -36,7 +36,7 @@ const enCareerData = [
     year: 2022,
     title: "Web Application Developer and Systems Administrator",
     subtitle: "Liceo Casino of Pontevedra",
-    details: "Focused on website creation and maintenance, computer equipment repair, data retrieval, along with proficient implementation of backup and recovery solutions, offering network support and managing email data migrations.",
+    details: "More than 1500 users. Focused on website creation and maintenance, computer equipment repair, data retrieval, along with proficient implementation of backup and recovery solutions, offering network support and managing email data migrations.",
     techs: ['Backups', 'HTML', 'CSS', 'JavaScript', 'Vite', 'NPM', 'React.js', 'Tailwind', 'phpMyAdmin', 'MongoDB', 'MariaDB', 'MySQL', 'Node.js', 'Git', 'GitHub', 'ESLint', 'WordPress', 'SEO', 'Bash', 'PowerShell']
   },
   {
@@ -44,7 +44,7 @@ const enCareerData = [
     year: 2023,
     title: "Web Application Developer",
     subtitle: "Fiverr, Upwork, Freelancer, Arc",
-    details: "Web development with expertise in creating dynamic and functional websites, bug fixing, and performance enhancement. Specialized in responsive design and code optimization for an exceptional user experience.",
+    details: "Docens of clients online. Web development with expertise in creating dynamic and functional websites, bug fixing, and performance enhancement. Specialized in responsive design and code optimization for an exceptional user experience.",
     techs: ['JavaScript', 'MongoDB', 'Express.js', 'React', 'Node.js']
   }
   
--- src/assets/data/career/esCareerData.js
+++ src/assets/data/career/esCareerData.js
@@ -4,7 +4,7 @@ const esCareerData = [
     year: 2017,
     title: 'Técnico Informático',
     subtitle: 'Puerto de Marín - Autoridad Portuaria de Marín y Ría de Pontevedra',
-    details: 'Implementé y desplegué servicios e instalaciones automatizadas, configuré y monitoreé sistemas y desarrollé scripts. Gestioné la migración de datos, instalé sistemas operativos y doté de asistencia técnica a usuarios.',
+    details: 'Más de 50 dispositivos informáticos. Implementé y desplegué servicios e instalaciones automatizadas, configuré y monitoreé sistemas y desarrollé scripts. Gestioné la migración de datos, instalé sistemas operativos y doté de asistencia técnica a usuarios.',
     techs: ['WDS', 'Zabbix', 'PowerShell', 'Backups', 'Ubuntu Server', 'Windows Server']
   },
   {
@@ -12,7 +12,7 @@ const esCareerData = [
     year: 2018,
     title: 'Técnico de Sistemas Informáticos',
     subtitle: 'Plexus Tech - Área Sanitaria de Pontevedra y O Salnés',
-    details: 'Responsable de la tutoría y supervisión de estudiantes en prácticas, mantenimiento de usuarios en entorno Windows, configuración de terminales médicos, despliegue de imágenes y migración de datos. Además, proporcioné soporte técnico y resolví incidencias a usuarios en el entorno laboral.',
+    details: 'Más de 1000 dispositivos informáticos. Responsable de la tutoría y supervisión de estudiantes en prácticas, mantenimiento de usuarios en entorno Windows, configuración de terminales médicos, despliegue de imágenes y migración de datos. Además, proporcioné soporte técnico y resolví incidencias a usuarios en el entorno laboral.',
     techs: ['AD-DS', 'Backups', 'TCP/IP']
   },
   {
@@ -20,7 +20,7 @@ const esCareerData = [
     year: 2020,
     title: 'Desarrollador y Administrador Web',
     subtitle: 'Depot230',
-    details: 'Configuré y administré plugins para WordPress, demostré competencias en programación PHP, manejé servicios web y operaciones con cPanel. Proporcioné entrenamiento y tutorización a clientes, desarrollé sitios web, además diseñé y edité imágenes publicitarias y logos.',
+    details: 'Más de 250 sitios web. Configuré y administré plugins para WordPress, demostré competencias en programación PHP, manejé servicios web y operaciones con cPanel. Proporcioné entrenamiento y tutorización a clientes, desarrollé sitios web, además diseñé y edité imágenes publicitarias y logos.',
     techs: ['Wordpress', 'PHP', 'cPanel', 'SEO', 'phpMyAdmin', 'HTML', 'CSS', 'JavaScript', 'GIMP']
   },
   {
@@ -28,7 +28,7 @@ const esCareerData = [
     year: 2021,
     title: 'Desarrollador de Aplicaciones Web y Administrador de Sistemas',
     subtitle: 'Liceo Casino de Pontevedra',
-    details: 'Desde el desarrollo de aplicaciones y consultoría IT hasta la recuperación de datos y mantenimiento de hardware. Además llevé a cabo las estrategias de copias de seguridad y restauración, ofrecí asistencia técnica de red y gestioné la migración del servicio de email.',
+    details: 'Más de 1500 Usuarios. Desde el desarrollo de aplicaciones y consultoría IT hasta la recuperación de datos y mantenimiento de hardware. Además llevé a cabo las estrategias de copias de seguridad y restauración, ofrecí asistencia técnica de red y gestioné la migración del servicio de email.',
     techs: ['Backups', 'HTML', 'CSS', 'JavaScript', 'Vite', 'NPM', 'React.js', 'Tailwind', 'phpMyAdmin', 'MongoDB', 'MariaDB', 'MySQL', 'Node.js', 'Git', 'GitHub', 'ESLint', 'WordPress', 'SEO', 'Bash', 'PowerShell']
   },
   {
@@ -36,7 +36,7 @@ const esCareerData = [
     year: 2022,
     title: "Desarrollador de Aplicaciones Web y Administrador de Sistemas",
     subtitle: "Liceo Casino de Pontevedra",
-    details: "Enfocado en la creación y mantenimiento de sitios web, reparación de equipos informáticos, recuperación de datos, junto con la implementación de copias de seguridad periódicas y sistemas de recuperación de datos, ofreciendo asistencia de red y gestión de migración de email.",
+    details: "Más de 1500 Usuarios. Enfocado en la creación y mantenimiento de sitios web, reparación de equipos informáticos, recuperación de datos, junto con la implementación de copias de seguridad periódicas y sistemas de recuperación de datos, ofreciendo asistencia de red y gestión de migración de email.",
     techs: ['Backups', 'HTML', 'CSS', 'JavaScript', 'Vite', 'NPM', 'React.js', 'Tailwind', 'phpMyAdmin', 'MongoDB', 'MariaDB', 'MySQL', 'Node.js', 'Git', 'GitHub', 'ESLint', 'WordPress', 'SEO', 'Bash', 'PowerShell']
   },
   {
@@ -44,7 +44,7 @@ const esCareerData = [
     year: 2023,
     title: "Desarrollador de Aplicaciones Web",
     subtitle: "Fiverr, Upwork, Freelancer, Arc",
-    details: "Desarrollo web con experiencia en la creación de sitios web dinámicos y funcionales, corrección de errores y mejora del rendimiento. Especializado en diseño responsivo, corrección de errores y optimización de código para una experiencia de usuario excepcional.",
+    details: "Docenas de clientes online. Desarrollo web con experiencia en la creación de sitios web dinámicos y funcionales, corrección de errores y mejora del rendimiento. Especializado en diseño responsivo, corrección de errores y optimización de código para una experiencia de usuario excepcional.",
     techs: ['JavaScript', 'MongoDB', 'Express.js', 'React', 'Node.js']
   }
 
